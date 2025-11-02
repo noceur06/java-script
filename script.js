de8a2ad17ff842af73f9544bee88ee686d6f413b
noceur06 <ya0558494@gmail.com>
2025-11-02 19:36:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+//VARIABLES
+
+
+//fullName = "yasar ali";//there is no space allow in variables in space only letters , digit , underscore and doller aloowed
+// age = 25;
+// price = 250;
+// x= null;
+// y = undefined;
+// isFollow = true;//there is no space allow in variables only letter , underscore or $(doller) should be the 1st charachter like below
+
+// fullName = 'yasar ali'; // its camel case we always use it everywhere
+// _fullname = "yasar ali";
+// $fullname = "yasar ali";
+
+
+//console.log(_fullname);  // only c is reserved to be print no C and reserved word can not be variables name
+
+
+
+
+// we should always use a keyword to use our variables like let and const ... var can be a keyword too but we should never use it
+//let fullName="yasar ali";// in let keyword if you dont give value to variable like "let a;" then in other line if you give it a value it will show as good
+//  let age = 25;
+// let price = 220;
+
+// console.log(fullName);
+
+// var age = 22;
+// var age = 25;
+// var age = 20; we should nevr use var cuz it can redeclare values
+
+
+// console.log(age);
+
+// and in const keyword we say that this value is constant it can not be changes
+
+const profile = {
+    userName: "yasar",
+    followers:84,
+    following:87,
+    isFollow: true
+};
+
+profile["userName"] = "yasar ali"
+
+// console.log(profile["followers"]);
+// console.log(profile.followers);
+console.log(profile.userName);
+
+
+
+
+
+
